5f3bea4ec7e431909f223c018ba4c8160f2a1352
jasminrainer <wi23b034@technikum-wien.at>
2024-06-14 13:53:16 +0200

    game ending notification + test for player class added

---
+++ src/test/java/org/example/PlayerTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+    private Player playerX;
+    private Player playerO;
+
+    @BeforeEach
+    public void setUp() {
+        playerX = new Player('X');
+        playerO = new Player('O');
+    }
+
+    @Test
+    public void testGetMarker() {
+        assertEquals('X', playerX.getMarker());
+        assertEquals('O', playerO.getMarker());
+    }
+
+    @Test
+    public void testPlayerMarkersNotEqual() {
+        assertNotEquals(playerX.getMarker(), playerO.getMarker());
+    }
+}
